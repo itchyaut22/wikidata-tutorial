ff1d8605d987a689cf5df42e6bac17934e5d9d8e
Martin Pfoser <pfoser.martin@gmail.com>
2015-06-01 15:26:20 +0200

    documentation

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,6 +1,14 @@
 'use strict';
 
 /**
+ * wbgetentities:
+ * Module to get the content of several entities, possibly with only parts of the entity included.
+ * Entities that are checked for existence, but not found, are reported (bugzilla:45509).
+ * During lookup both sites and titles can be a list, that is if the entities are items, but the shorter one is cycled to match the longer one.
+ * If one of the lists contain a single entry it will be used for all entries from the other list.
+ * If both lists are equally long the sitelinks will be well-formed pairs.
+ *
+ *
  * @ngdoc function
  * @name tutorialWikidataApp.controller:MainCtrl
  * @description
@@ -10,6 +18,15 @@
 angular.module('tutorialWikidataApp')
   .controller('MainCtrl', function($scope, $http, localStorageService) {
 
+    // some wikidata constants
+    var WIKIDATA_PROPERTY_END_DATE            = 'P582';
+    var WIKIDATA_PROPERTY_CAPITAL             = 'P36';
+    var WIKIDATA_PROPERTY_POPULATION          = 'P1082';
+    var WIKIDATA_PROPERTY_CURRENCY            = 'P38';
+    var WIKIDATA_PROPERTY_MEMBER_OF           = 'P463';
+    var WIKIDATA_PROPERTY_SHARES_BORDER_WITH  = 'P47';
+
+    // scope variables initialization
     $scope.selectedCountry = {};
     $scope.countries = [];
     $scope.result = [];
@@ -20,14 +37,42 @@ angular.module('tutorialWikidataApp')
     $scope.result.memberOf = [];
     $scope.result.sharesBorderWith = [];
 
+    /**
+    * Function for generating wikidata query string using wikidata api
+    */
+    var getWikidataUrlForItem = function(item, props) {
+      var itemStr = '';
+
+      if (!angular.isString(item)) {
+        itemStr = item.toString();
+      } else {
+        itemStr = item;
+      }
+
+      if (itemStr.indexOf('Q') == 0) {
+      } else {
+        itemStr = 'Q' + itemStr;
+      }
+
+      if (angular.isUndefined(props)) {
+        props = 'labels';
+      } else {
+        props = props.join('|');
+      }
+      return 'https://www.wikidata.org/w/api.php?action=wbgetentities&format=json&ids=' + itemStr + '&props=' + props + '&languages=de&callback=JSON_CALLBACK';
+    };
+
+    /**
+    * Function for determine countries that shares the border with the country
+    */
     var getShareBorder = function(shareBorder) {
       angular.forEach(shareBorder, function(member) {
 
         var hasQualifiers = !angular.isUndefined(member.qualifiers);
-        if (!hasQualifiers || angular.isUndefined(member.qualifiers['P582'])) {
+        if (!hasQualifiers || angular.isUndefined(member.qualifiers[WIKIDATA_PROPERTY_END_DATE])) {
 
           var id = member.mainsnak.datavalue.value['numeric-id'];
-          var dataStr = 'https://www.wikidata.org/w/api.php?action=wbgetentities&format=json&ids=Q' + id + '&props=labels&languages=de&callback=JSON_CALLBACK';
+          var dataStr = getWikidataUrlForItem(id);
 
           $http({
               url: dataStr,
@@ -44,13 +89,17 @@ angular.module('tutorialWikidataApp')
       });
     };
 
+    /**
+    * Function for determine organizations that the country has a membership with
+    */
     var getMemberOf = function(memberOf) {
       angular.forEach(memberOf, function(member) {
 
         var hasQualifiers = !angular.isUndefined(member.qualifiers);
-        if (!hasQualifiers || angular.isUndefined(member.qualifiers['P582'])) {
+        if (!hasQualifiers || angular.isUndefined(member.qualifiers[WIKIDATA_PROPERTY_END_DATE])) {
+
           var id = member.mainsnak.datavalue.value['numeric-id'];
-          var dataStr = 'https://www.wikidata.org/w/api.php?action=wbgetentities&format=json&ids=Q' + id + '&props=labels&languages=de&callback=JSON_CALLBACK';
+          var dataStr = getWikidataUrlForItem(id);
 
           $http({
               url: dataStr,
@@ -66,12 +115,15 @@ angular.module('tutorialWikidataApp')
 
     };
 
+    /**
+    * Function for determine the country's currency
+    */
     var getCurrency = function(currencyId) {
       angular.forEach(currencyId, function(response) {
         var hasQualifiers = !angular.isUndefined(response.qualifiers);
-        if (!hasQualifiers || angular.isUndefined(response.qualifiers['P582'])) {
+        if (!hasQualifiers || angular.isUndefined(response.qualifiers[WIKIDATA_PROPERTY_END_DATE])) {
           var mCurrencyId = response.mainsnak.datavalue.value['numeric-id'];
-          var dataStr = 'https://www.wikidata.org/w/api.php?action=wbgetentities&format=json&ids=Q' + mCurrencyId + '&props=labels&languages=de&callback=JSON_CALLBACK';
+          var dataStr = getWikidataUrlForItem(mCurrencyId);
           $http({
               url: dataStr,
               method: 'jsonp'
@@ -85,6 +137,9 @@ angular.module('tutorialWikidataApp')
       });
     };
 
+    /**
+    * Function for determine the country's population
+    */
     var getPopulation = function(population) {
       var intNr = parseInt(population);
 
@@ -93,15 +148,20 @@ angular.module('tutorialWikidataApp')
       } else {
         $scope.result.population = 'NaN';
       }
-
     };
 
+    /**
+    * Function for determine the country's capial
+    */
     var getCapital = function(capitalId) {
       angular.forEach(capitalId, function(response) {
         var hasQualifiers = !angular.isUndefined(response.qualifiers);
-        if (!hasQualifiers || angular.isUndefined(response.qualifiers['P582']) || (!angular.isUndefined(response.qualifiers['P582']) && response.qualifiers['P582'][0].snaktype == 'novalue')) {
+        if (!hasQualifiers || angular.isUndefined(response.qualifiers[WIKIDATA_PROPERTY_END_DATE]) ||
+           (!angular.isUndefined(response.qualifiers[WIKIDATA_PROPERTY_END_DATE]) &&
+             response.qualifiers[WIKIDATA_PROPERTY_END_DATE][0].snaktype == 'novalue')) {
+
           var mCapitalId = response.mainsnak.datavalue.value['numeric-id'];
-          var dataStr = 'https://www.wikidata.org/w/api.php?action=wbgetentities&format=json&ids=Q' + mCapitalId + '&props=labels&languages=de&callback=JSON_CALLBACK';
+          var dataStr = getWikidataUrlForItem(mCapitalId);
           $http({
               url: dataStr,
               method: 'jsonp'
@@ -112,13 +172,15 @@ angular.module('tutorialWikidataApp')
               $scope.result.capital = capital;
             });
         }
-
-
       });
     };
 
+    /**
+    * ActionListener: Button 'Abfrage starten' clicked
+    * Call all above mentioned functions to determine some infos from a wikidata country
+    */
     $scope.startQuery = function() {
-      var dataStr = 'https://www.wikidata.org/w/api.php?action=wbgetentities&format=json&ids=' + $scope.selectedCountry + '&props=claims|labels&languages=de&callback=JSON_CALLBACK';
+      var dataStr = getWikidataUrlForItem($scope.selectedCountry, ['claims', 'labels']);
       $http({
           url: dataStr,
           method: 'jsonp'
@@ -145,24 +207,24 @@ angular.module('tutorialWikidataApp')
             officialName = response.entities[country].labels.de.value;
             $scope.result.country = officialName;
           }
-          if (!angular.isUndefined(response.entities[country].claims.P36)) {
-            capital = response.entities[country].claims.P36;
+          if (!angular.isUndefined(response.entities[country].claims[WIKIDATA_PROPERTY_CAPITAL])) {
+            capital = response.entities[country].claims[WIKIDATA_PROPERTY_CAPITAL];
             getCapital(capital);
           }
-          if (!angular.isUndefined(response.entities[country].claims.P1082)) {
-            population = response.entities[country].claims.P1082[0].mainsnak.datavalue.value['amount'].substring(1);
+          if (!angular.isUndefined(response.entities[country].claims[WIKIDATA_PROPERTY_POPULATION])) {
+            population = response.entities[country].claims[WIKIDATA_PROPERTY_POPULATION][0].mainsnak.datavalue.value['amount'].substring(1);
             getPopulation(population);
           }
-          if (!angular.isUndefined(response.entities[country].claims.P38)) {
-            currency = response.entities[country].claims.P38;
+          if (!angular.isUndefined(response.entities[country].claims[WIKIDATA_PROPERTY_CURRENCY])) {
+            currency = response.entities[country].claims[WIKIDATA_PROPERTY_CURRENCY];
             getCurrency(currency);
           }
-          if (!angular.isUndefined(response.entities[country].claims.P463)) {
-            memberOf = response.entities[country].claims.P463;
+          if (!angular.isUndefined(response.entities[country].claims[WIKIDATA_PROPERTY_MEMBER_OF])) {
+            memberOf = response.entities[country].claims[WIKIDATA_PROPERTY_MEMBER_OF];
             getMemberOf(memberOf);
           }
-          if (!angular.isUndefined(response.entities[country].claims.P47)) {
-            sharesBorder = response.entities[country].claims.P47;
+          if (!angular.isUndefined(response.entities[country].claims[WIKIDATA_PROPERTY_SHARES_BORDER_WITH])) {
+            sharesBorder = response.entities[country].claims[WIKIDATA_PROPERTY_SHARES_BORDER_WITH];
             getShareBorder(sharesBorder);
           }
 
@@ -186,10 +248,11 @@ angular.module('tutorialWikidataApp')
           var itemsWithQ = [];
           var index;
 
+          // max 50 items allowed 
           for (index = 0; index < 50; ++index) {
             itemsWithQ[index] = 'Q' + items[index];
           }
-          var wikidatastr = 'http://www.wikidata.org/w/api.php?action=wbgetentities&format=json&ids=' + itemsWithQ.join('|') + '&props=labels&languages=de&callback=JSON_CALLBACK';
+          var wikidatastr = getWikidataUrlForItem(itemsWithQ.join('|'));
           $http({
               url: wikidatastr,
               method: 'jsonp'
@@ -219,6 +282,7 @@ angular.module('tutorialWikidataApp')
       $scope.countries = countriesInStore;
       $scope.selectedCountry = countriesInStore[0].value;
     } else {
+      // no cookies available --> query all countries from wdq.wmflabs.org api
       loadCountries();
     }
   });
